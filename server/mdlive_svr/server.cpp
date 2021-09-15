aaf123af331e774809555d37021e59c166f56973
tristan <wangwl1980@gmail.com>
2021-09-15 10:05:59 +0800

    fixed bug

--- server/mdlive_svr/server.cpp
+++ server/mdlive_svr/server.cpp
@@ -43,18 +43,18 @@ using namespace MdLive;
 #define __CONFIG_FILE__   "./mdlive_config.ini"
 
 CThostFtdcMdApi* pMd;
-CThostFtdcMdSpiI* MdSpi; 
+CThostFtdcMdSpiI* MdSpi;
 
 //记录已经推送过的合约对应的分k
 map<uint64_t, long> instrument_key_map;	//合约--key映射表   推送过的kline的记录
 //记录已经推送过的日k
-map<string, long> instrument_daybar_map;	
+map<string, long> instrument_daybar_map;
 
 void update_daybar(CThostFtdcDepthMarketDataField* pnew, uint64_t Timestamp, string productid)
 {
 	DayBar day;
-	day.Timestamp           = Timestamp;        
-	day.High                = pnew->HighestPrice;           
+	day.Timestamp           = Timestamp;
+	day.High                = pnew->HighestPrice;
 	day.Low                 = pnew->LowestPrice;
 	day.Open                = pnew->OpenPrice;
 	day.Close               = pnew->ClosePrice;
@@ -71,9 +71,9 @@ void update_daybar(CThostFtdcDepthMarketDataField* pnew, uint64_t Timestamp, str
 	day.LowerLimit          = pnew->LowerLimitPrice;
 
 	day.Close = day.Price;
-	
+
 	Singleton<CDayBar>::Instance().set(pnew->InstrumentID, pnew->TradingDay, productid, day);
-	
+
 	//push给mdindex
 //	SessionM::Instance().send_day_snap_real(pnew->TradingDay, pnew->InstrumentID, day);
 }
@@ -82,7 +82,7 @@ void update_daybar(CThostFtdcDepthMarketDataField* pnew, uint64_t Timestamp, str
 void push_DepthMarketData(CThostFtdcDepthMarketDataField* pnew)
 {
 	static CM::DepthMarketData mdata;
-	mdata.TradingDay			= pnew->TradingDay			; 
+	mdata.TradingDay			= pnew->TradingDay			;
 	mdata.InstrumentID			= pnew->InstrumentID        ;
 	mdata.ExchangeID			= pnew->ExchangeID          ;
 	mdata.ExchangeInstID		= pnew->ExchangeInstID      ;
@@ -140,7 +140,7 @@ Bar get_1minute_bar(string instrument, InstrumentData* p, int64_t sendtime)
     }
     else
     {
-        
+
         double close = 0, io = 0;
         Bar* b = GetPrevBar<Bar>(p, sendtime, K1MINUTE);
         if(b == NULL)
@@ -156,13 +156,13 @@ Bar get_1minute_bar(string instrument, InstrumentData* p, int64_t sendtime)
             io    = b->OpenInterest;
         }
         Bar t;
-        t.Timestamp     = sendtime;   
-        t.High          = close;        
-        t.Low           = close;         
-        t.Open          = close;        
-        t.Close         = close;       
-        t.Volume        = 0;      
-        t.Turnover      = 0;    
+        t.Timestamp     = sendtime;
+        t.High          = close;
+        t.Low           = close;
+        t.Open          = close;
+        t.Close         = close;
+        t.Volume        = 0;
+        t.Turnover      = 0;
         t.OpenInterest  = io;
         return t;
     }
@@ -191,23 +191,23 @@ void process_period_kline(string tradingday, string instrument, InstrumentData* 
 
         int64_t key = stoint64(instrument.c_str())  + period;
 		map<uint64_t, long>::iterator it = instrument_key_map.find(key);
-        if(it == instrument_key_map.end()) 
+        if(it == instrument_key_map.end())
         {
             instrument_key_map[key] =  sendtime; //第一次进来就赋值了上个需要推送的数据21:03
             continue;
         }
 
         if(sendtime <= 0 || now == 0 || sendtime <= it->second) continue;
-        
+
         long bt = sendtime;
         now = sendtime + period*60*1000;
         //非周期的倍数进不来？？？
         Bar k = get_1minute_bar(instrument, p, sendtime);
-        sendtime += 60*1000; 
+        sendtime += 60*1000;
         while(sendtime < now)
         {
             Bar* pk = GetBar<Bar>(p, sendtime,  K1MINUTE, false);
-            if(pk == NULL) 
+            if(pk == NULL)
             {
                 sendtime += 60*1000;
                 continue;
@@ -215,8 +215,8 @@ void process_period_kline(string tradingday, string instrument, InstrumentData* 
             if(pk->High > k.High) k.High = pk->High;
             if(pk->Low > 0.1 && (pk->Low < k.Low || k.Low < 0.1)) k.Low = pk->Low;
             if(pk->Close > 0.1) k.Close = pk->Close;
-            k.Volume        += pk->Volume;      
-            k.Turnover      += pk->Turnover;    
+            k.Volume        += pk->Volume;
+            k.Turnover      += pk->Turnover;
             k.OpenInterest  += pk->OpenInterest;
             //LOG_DEBUG("test instrument=" << instrument << "  time=" << t-period*60*1000 << " addkline=" << sendtime << " period=" << period);
             sendtime += 60*1000;
@@ -241,28 +241,28 @@ void check_and_push_kline(long Timestamp, string tradingday)
         //sendtime就是当前要推送的
         //没开盘不退送 目的把收盘的这根线放到下次开盘推送   收盘了必须推送。
         string product = itr->second->instrucment.ProductID; //get_productid(itr->first);
-        if(!g_timeserise.isTrading((char*)product.c_str(), Timestamp/1000, 120)) 
+        if(!g_timeserise.isTrading((char*)product.c_str(), Timestamp/1000, 120))
             continue;
 
         process_period_kline(tradingday, itr->first, itr->second, Timestamp);
-        
+
 
         //需要计算的是上一个，   上一个可能不存在就不推送  可能已经推送过了。
         time_t sendtime = 0;
         long now = g_timeserise.GetSeriseKey((char*)product.c_str(), 1, strtostamp(tradingday.c_str(), "%Y%m%d"), Timestamp/1000, sendtime);
         sendtime = sendtime * 1000;
-       
+
         //不存在就设置
         int64_t key = stoint64(itr->first.c_str())  + 1;
 		map<uint64_t, long>::iterator it = instrument_key_map.find(key);
 		if(it == instrument_key_map.end())
         {
-            instrument_key_map[key] =  Timestamp-60; 
+            instrument_key_map[key] =  Timestamp-60;
             continue;
         }
-        
+
         if(sendtime <= 0 || now == 0 || sendtime <= it->second) continue;
-        
+
         Bar k = get_1minute_bar(itr->first, itr->second, sendtime);
 	    SessionM::Instance().send_kline(tradingday,  itr->first, k, 1);
 	    instrument_key_map[key] =  sendtime;
@@ -291,8 +291,8 @@ void check_and_push_daybar( string tradingday)
 				LOG_DEBUG("DayBar InstrumentID="<<itr->first
 						<< " TradingDay=" << tradingday
 						<< " Timestamp=" << k->Timestamp
-						<< " Close=" <<k->Close 
-						<< " Settlement=" << k->Settlement 
+						<< " Close=" <<k->Close
+						<< " Settlement=" << k->Settlement
 						<< " voulume=" << k->Volume);
 			}
 		}
@@ -303,14 +303,14 @@ void check_and_push_daybar( string tradingday)
 int Run(const char* pin, uint32_t len)
 {
 	CThostFtdcDepthMarketDataField* pnew = (CThostFtdcDepthMarketDataField*)pin;
-	LOG_INFO("-------------->" << pnew->InstrumentID)
+	// LOG_INFO("-------------->" << pnew->InstrumentID)
     if(strlen(pnew->InstrumentID) == 0)
 	{
 		LOG_ERROR("InstrumentID:" << pnew->InstrumentID);
 		return 0;
 	}
 
-	
+
     //加一个本地时间的限制 不接收行情时间
 	uint32_t tExchange = MdSpi->GetExchangeTime(pnew->ExchangeID);
 	if(g_tradetime.IsStopRcvData(tExchange, "SHFE"))
@@ -339,10 +339,10 @@ int Run(const char* pin, uint32_t len)
 	}
 
     LOG_DEBUG(pnew->TradingDay << " " << pnew->ActionDay << " " << pnew->InstrumentID << " " << prev->instrucment.ExchangeID << " " << pnew->UpdateMillisec);
-	
+
     //推送原始数据
 	push_DepthMarketData(pnew);
-		
+
 	char timebuf[32] = {0};
 	snprintf(timebuf, sizeof(timebuf), "%s %s", pnew->ActionDay, pnew->UpdateTime);
 	struct tm _tm = strtotm(timebuf, "%Y%m%d %H:%M:%S"); //当前时间
@@ -354,8 +354,8 @@ int Run(const char* pin, uint32_t len)
     //LOG_INFO("Timestamp=" << Timestamp << " t=" << tt);
 
     Bar* k = GetBar<Bar>(prev, Timestamp,  K1MINUTE, false); //获取当前kline NULL 表示新的
-	Bar* kprev = GetPrevBar<Bar>(prev, Timestamp,  K1MINUTE); 
-	if(k==NULL && kprev!=NULL) 
+	Bar* kprev = GetPrevBar<Bar>(prev, Timestamp,  K1MINUTE);
+	if(k==NULL && kprev!=NULL)
 	{
         int64_t key = stoint64(pnew->InstrumentID)  + 1;
 		map<uint64_t, long>::iterator itr = instrument_key_map.find(key);
@@ -377,25 +377,25 @@ int Run(const char* pin, uint32_t len)
 	if(pnew->Volume > prev->snap.Volume && IsDValid(pnew->LastPrice) && pnew->Turnover > prev->snap.Turnover)
 	{
 		calculate_Kline(prev, timebuf, pnew->LastPrice, pnew->Volume - prev->snap.Volume, pnew->Turnover-prev->snap.Turnover, pnew->OpenInterest);
-		
-		if(string(pnew->InstrumentID).find("0001") != string::npos) 
+
+		if(string(pnew->InstrumentID).find("0001") != string::npos)
 		{
 			LOG_DEBUG("prev->snap.Turnover=" << prev->snap.Turnover << " pnew->Turnover=" << pnew->Turnover);
 		}
 	}
 
 	//计算ticker
-	if(IsDValid(pnew->LastPrice) && (pnew->Volume > prev->snap.Volume || 
-			(pnew->Volume == prev->snap.Volume && ((IsDValid(pnew->BidPrice1) && !IsEqDouble(pnew->BidPrice1, prev->snap.BidPrice1)) || 
-			pnew->BidVolume1	!= prev->snap.BidVolume1 || 
-			(IsDValid(pnew->AskPrice1) && !IsEqDouble(pnew->AskPrice1, prev->snap.AskPrice1))  || 
+	if(IsDValid(pnew->LastPrice) && (pnew->Volume > prev->snap.Volume ||
+			(pnew->Volume == prev->snap.Volume && ((IsDValid(pnew->BidPrice1) && !IsEqDouble(pnew->BidPrice1, prev->snap.BidPrice1)) ||
+			pnew->BidVolume1	!= prev->snap.BidVolume1 ||
+			(IsDValid(pnew->AskPrice1) && !IsEqDouble(pnew->AskPrice1, prev->snap.AskPrice1))  ||
 			pnew->AskVolume1	!= prev->snap.AskVolume1))))
 	{
 		Ticker* k = calculate_Ticker(prev,  &prev->snap, pnew);
 		SessionM::Instance().send_ticker(pnew->TradingDay, pnew->InstrumentID, *k);
-	}	
+	}
 
-	
+
 	//这里是实时日k的处理 更新db线程会定时更新
 	if((pnew->Volume >= prev->snap.Volume) && IsDValid(pnew->LastPrice))
 	{
@@ -468,10 +468,10 @@ class ProcessDataThread : public IceUtil::Thread
 				if(!g_holidays.IsTrading(time(NULL)))
 				{
 					while(md_fifo_pop((char*)&tmp, sizeof(CThostFtdcDepthMarketDataField))==0);
-					
+
 					static int count_check = 0;
 					if(count_check++ % 1000 == 0) LOG_INFO("holiday stop recv data!");
-				
+
 					sleep(1);
 					continue;
 				}
@@ -480,14 +480,14 @@ class ProcessDataThread : public IceUtil::Thread
 				{
 					Run((const char*)&tmp, sizeof(CThostFtdcDepthMarketDataField));
 				}
-			
+
 				//日k推送 收盘15.30进行推送
 				uint32_t tExchange = MdSpi->GetExchangeTime("CFFEX");
 				uint32_t status = g_tradetime.GetMarketStatus(tExchange, "SHFE");
 				bool change = g_tradetime.OnChangeMarketStatus( "SHFE", status);
-	
+
                 //下午结算时间15:16---15:30  推送最后的15：15的行情
-				if(g_tradetime.IsSettlement(status) && change)	
+				if(g_tradetime.IsSettlement(status) && change)
 				{
                     struct tm tmp = {0};
                     time_t tt = time(NULL);
@@ -496,16 +496,16 @@ class ProcessDataThread : public IceUtil::Thread
 	                check_and_push_kline(Timestamp, tradingday); //15:16:00
                     LOG_INFO("settlement push Timestamp=" << Timestamp << " tradingday=" << tradingday);
                 }
-			
+
                 //15:30
-				if(g_tradetime.IsCloseMarket(status) && change)	
+				if(g_tradetime.IsCloseMarket(status) && change)
 				{
 					check_and_push_daybar(tradingday);
 					g_mapInstrmentData.clear();
 					LOG_INFO("market status change to 10 close market..");
 				}
 
-				if(tradingday < MdSpi->GetTradingDay()) 
+				if(tradingday < MdSpi->GetTradingDay())
 				{
 					set<string> idlist;
 					QueryMongoInstrument(g_conf, idlist); //这个里面会更新合约信息的
@@ -523,13 +523,13 @@ class ProcessDataThread : public IceUtil::Thread
 						itr->second->Init();
 						LOG_DEBUG("instrument=" << itr->first << " volumemultipl=" << (itr->second->instrucment.VolumeMultiple) << " exchangeid="<<(itr->second->instrucment.ExchangeID));
 					}
-					
+
 					instrument_key_map.clear();
 					instrument_daybar_map.clear();
                     g_timeserise.clear();
 
 					Singleton<CDayBar>::Instance().clear();
-					
+
 					LOG_INFO("old tradingday=" << tradingday << " new tradingday="<< MdSpi->GetTradingDay() << " instrument size=" << g_mapInstrmentData.size());
 					//重新订阅
 					MdSpi->ChangeSubcribe(idlist);
@@ -556,7 +556,7 @@ int main(int argc, char* argv[])
 {
 
 	int ret = ::InitAll(__CONFIG_FILE__);
-	if(ret < 0) 
+	if(ret < 0)
 	{
 		printf("init error \n");
 		exit(-1);
@@ -567,10 +567,10 @@ int main(int argc, char* argv[])
 	{
 		return -1;
 	}
-    
+
 	//写mongo模块
 	Singleton<CDayBar>::Instance().init(g_conf);
-	
+
 	g_tradetime.Init(CMongodb::Instance());
 	g_holidays.Init(CMongodb::Instance());
     g_timeserise.Init(CMongodb::Instance());
@@ -580,13 +580,13 @@ int main(int argc, char* argv[])
 	g_conf.get<string>("CTP", "pwd", pwd, "");
 	g_conf.get<string>("CTP", "brokerid", brokerid, "");
 	g_conf.get<string>("CTP", "ctpurl", ctpurl, "");
-	
+
 	pMd = CThostFtdcMdApi::CreateFtdcMdApi();
 	MdSpi = new CThostFtdcMdSpiI(pMd, brokerid, account, pwd, idlist, &md_fifo_push);
 	pMd->RegisterSpi(MdSpi);
 	pMd->RegisterFront((char*)ctpurl.c_str());
 	pMd->Init();
-	
+
 #ifdef ICE_STATIC_LIBS
 	Ice::registerIceSSL();
 #endif
@@ -594,7 +594,7 @@ int main(int argc, char* argv[])
 	string ip, port;
 	g_conf.get<string>("mdserver", "ip", ip, "127.0.0.1");
 	g_conf.get<string>("mdserver", "port", port, "10001");
-	
+
 	Ice::InitializationData initData;
 	stringstream ss;
 	ss << "tcp -h " << ip << " -p " << port;
@@ -603,7 +603,7 @@ int main(int argc, char* argv[])
 	initData.properties->setProperty("Ice.Warn.Connections", "1");
 	initData.properties->setProperty("Ice.Trace.Network", "0");
 	initData.properties->setProperty("Ice.Trace.Protocol", "0");
-	
+
 	initData.properties->setProperty("Ice.ThreadPool.Server.Size", "5");
 	initData.properties->setProperty("Ice.ThreadPool.Server.SizeMax", "100");
 
@@ -620,8 +620,8 @@ int MdServer::run(int argc, char* argv[])
 		//创建对象适配器
 		Ice::ObjectAdapterPtr adapter = communicator()->createObjectAdapter("MDLIVE.service");
 
-		MdSessionIPtr sender = new MdSessionI(); 
-	
+		MdSessionIPtr sender = new MdSessionI();
+
 		adapter->add(sender, communicator()->stringToIdentity("sender"));
 
 		IceUtil::ThreadPtr t = new ProcessDataThread;
@@ -651,9 +651,3 @@ int MdServer::run(int argc, char* argv[])
 	return EXIT_SUCCESS;
 
 }
-
-
-
-
-
-
